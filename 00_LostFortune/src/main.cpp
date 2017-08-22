9a1b99bc3cf7c0e117dc79e8dfac29d73949570b
Gaetz <gaetz@rufflerim.com>
2017-08-22 08:16:50 +0200

    Calculations

--- 00_LostFortune/src/main.cpp
+++ 00_LostFortune/src/main.cpp
@@ -3,9 +3,16 @@
 using std::cout;
 using std::endl;
 
-int main(int argc, char* argv[]) {
-
-    cout << "Game over!" << endl;
+int main(int argc, char *argv[])
+{
+    cout << "7 + 3 = " << 7 + 3 << endl;
+    cout << "7 - 3 = " << 7 - 3 << endl;
+    cout << "7 * 3 = " << 7 * 3 << endl;
+    cout << "7 / 3 = " << 7 / 3 << endl;
+    cout << "7.0 / 3.0 = " << 7.0 / 3.0 << endl;
+    cout << "7 % 3 = " << 7 % 3 << endl;
+    cout << "7 + 3 * 5 = " << 7 + 3 * 5 << endl;
+    cout << "(7 + 3) * 5 = " << (7 + 3) * 5 << endl;
 
     return 0;
 }
