436478a4b793a5f189c1eddaecac250996fb42fc
Gaetz <gaetz@rufflerim.com>
2017-08-22 13:59:15 +0200

    Guess My Number base program

---
+++ 01_GuestMyNumber/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main(int argc, char *argv[])
+{
+
+    // End of program
+    char end;
+    cout << "\nEnter a char to end >> ";
+    cin >> end;
+
+    return 0;
+}
